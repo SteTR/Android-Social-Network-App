037c2b4a4cb748fdf8cada89805a5275743cf5ba
Bryce Fujita <fujita63t@gmail.com>
2021-04-29 13:42:02 -0700

    Navigations fixed and the user may now navigate the app.

--- app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
@@ -18,12 +18,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    BottomNavigationView navView = findViewById(R.id.nav_view);
-    // Passing each menu ID as a set of Ids because each
-    // menu should be considered as top level destinations.
-    //mAppBarConfiguration = new AppBarConfiguration.Builder();
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_weather,
+                R.id.navigation_contacts, R.id.navigation_chats).build();
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }
 
 
     @Override
