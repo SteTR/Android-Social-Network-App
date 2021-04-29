1a8e6ae2b45b6c88a98f0f4f1525b4693eddd594
Bryce Fujita <fujita63t@gmail.com>
2021-04-29 09:58:17 -0700

    Added Sign In page.  Struggling to get dependencies for navigation to appear in Android studio.  Fails to build classes for Navigation Graphs.

--- app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
@@ -23,11 +23,7 @@ public class MainActivity extends AppCompatActivity {
     BottomNavigationView navView = findViewById(R.id.nav_view);
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
-//    mAppBarConfiguration = new AppBarConfiguration.Builder(
-//    R.id.navigation_home, R.id.navigation_weather, R.id.navigation_contacts, R.id.navigation_chats).build();
-//    NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-//        NavigationUI.setupWithNavController(navView, navController);
+    //mAppBarConfiguration = new AppBarConfiguration.Builder();
 
 
     @Override
