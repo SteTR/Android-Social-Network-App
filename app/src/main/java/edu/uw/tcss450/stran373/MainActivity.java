770e47e21581885fbef4de56db40ea7abe2cff73
Bryce Fujita <fujita63t@gmail.com>
2021-05-05 12:44:06 -0700

    Revert "J work"

--- app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
+++ app/src/main/java/edu/uw/tcss450/stran373/MainActivity.java
@@ -1,46 +1,14 @@
 package edu.uw.tcss450.stran373;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-/**
- * The inner activity used to house other functionalities (chat, contacts, etc.).
- */
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * The set of arguments this activity has (specified in main_graph).
-     */
-    private MainActivityArgs args;
-
-    /**
-     * Initializes the SuccessFragment for the user.
-     *
-     * @param theSavedInstanceState is a bundle object.
-     */
     @Override
-    protected void onCreate(Bundle theSavedInstanceState) {
-        super.onCreate(theSavedInstanceState);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        args = MainActivityArgs.fromBundle(getIntent().getExtras());
-        new ViewModelProvider(this,
-                new UserInfoViewModel.UserInfoViewModelFactory(args.getJwt(), args.getEmail())
-        ).get(UserInfoViewModel.class);
-
     }
-
-//    @Override
-//    public boolean onSupportNavigateUp() {
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
-//    }
-//
-//    public MainActivityArgs getTheArgs() {
-//        return args;
-//    }
 }
