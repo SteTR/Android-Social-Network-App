a831539f86d231703dd5c1cd17b05a74d5863f15
KamenResident <67707162+KamenResident@users.noreply.github.com>
2021-05-10 00:27:06 -0700

    Local Weather
    Starting to work on implementing local weather for the application.

--- app/src/main/java/edu/uw/tcss450/stran373/ui/Weather/WeatherFragment.java
+++ app/src/main/java/edu/uw/tcss450/stran373/ui/Weather/WeatherFragment.java
@@ -13,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.text.DecimalFormat;
+
 import edu.uw.tcss450.stran373.R;
 import edu.uw.tcss450.stran373.databinding.FragmentWeatherBinding;
 
@@ -23,11 +25,30 @@ public class WeatherFragment extends Fragment {
     /**Binding used to access resources for view.*/
     FragmentWeatherBinding binding;
 
+    private static final WeatherCard[] mCards = new WeatherCard[5];
+
+    private final String mWeatherURL = "http://api.openweathermap.org/data/2.5/weather";
+    private final String mAppID = "e53301e27efa0b66d05045d91b2742d3";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void getWeatherDetails(View view) {
+        String tempURL = "";
+        String city = "";
+        String state = "";
+        tempURL = mWeatherURL + "?q=" + city + "," + "USA" + "%appid=" + mAppID;
+
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         binding = FragmentWeatherBinding.inflate(inflater);
+
         final RecyclerView rv = binding.weatherRecyler;
         rv.setAdapter(new WeatherRecyclerViewAdapter(WeatherGenerator.getWeatherList()));
         return binding.getRoot();
